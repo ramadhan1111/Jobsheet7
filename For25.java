b95314800462e7158a51e94db542d6c94bd682ce
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-10-10 15:00:25 +0700

    Perulangan For

---
+++ For25.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class For25 {
+    public static void main(String[] args) {
+        Scanner input25 = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hasil kuadrat dari " + 1 + " adalah " + (i * i));
+        }
+    }
+}
