7306fd35e6e0f9a17c19ba540172b59958b9d4e8
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-10-10 15:56:03 +0700

    Pengulangan do while

---
+++ dowhile25.java
@@ -0,0 +1,16 @@
+public class dowhile25 {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+        System.out.println("Program berhenti");
+
+        //do while dengan nilai x = 10
+         //int x = 10;
+        //do {
+            //System.out.println(x);
+        //} while (++x <= 8);
+        //System.out.println("Program berhenti");
+    }
+}
